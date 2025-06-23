e76edb1320df0534a655cd9c1e42f7ca4f3f5a00
Sushant <sushantbhagat561@gmail.com>
2025-06-23 15:36:17 +0530

    creating a html page

--- home/views.py
+++ home/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Welcome to PopWorld!</h1><p>This is the home page.</p>')
+    return render(request, 'home.html')
